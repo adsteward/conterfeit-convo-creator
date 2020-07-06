63a2085d77e70ff754a1c127d39226940fc2408f
Adeline Steward-Nolan <asteward@macalester.edu>
2020-07-05 20:01:09 -0500

    day one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule}        from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ScreenComponent } from './screen/screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScreenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/screen/screen.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input } from '@angular/core';
+//import '../../assets/icons';
+
+@Component({
+  selector: 'app-screen',
+  templateUrl: './screen.component.html',
+  styleUrls: ['./screen.component.scss']
+})
+export class ScreenComponent implements OnInit {
+  @Input() sendee: string;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  get sendeeInitials(): string {
+    return "AS";
+  }
+
+  get texts(): any{
+    return [
+      { from: 'me', text: 'hey'},
+      { from: 'you', text: 'what'},
+      { from: 'me', text: 'omg'},
+      { from: 'me', text: 'how does this look?'},
+      { from: 'you', text: 'what about this longer line so long almost too long?'},
+      { from: 'me', text: 'bad'}
+    ]
+  }
+}
